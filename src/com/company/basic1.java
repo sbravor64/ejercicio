19c40ce9bc63a7fc3bb28f094dbb33b327445d93
sbravor64@elpuig.xeill.net <19972208>
2018-10-30 18:21:33 +0100

    ejercicio 1

--- src/com/company/basic1.java
+++ src/com/company/basic1.java
@@ -1,8 +1,25 @@
 package com.company;
 
-public class Main {
+import java.util.Scanner;
+
+public class basic1 {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Scanner año = new Scanner(System.in);
+        System.out.print("AÑO: ");
+        int year = año.nextInt();
+
+        if (year % 4 == 0) {
+            if (year % 100 == 0 && year % 4 == 0){
+                System.out.println("es bisiesto");
+            } else {
+                System.out.println("es bisiesto");
+            }
+
+        } else {
+            System.out.println("no es bisiesto");
+        }
     }
 }
+
