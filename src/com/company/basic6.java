5db4ea9db48721b0130d11ecaba0b117b3dad9c6
sbravor64@elpuig.xeill.net <19972208>
2018-11-06 17:53:02 +0100

    ejercicios por hacer

--- src/com/company/basic6.java
+++ src/com/company/basic6.java
@@ -6,9 +6,20 @@ public class basic6 {
 
     public static void main(String[] args) {
 
-        Scanner nota = new Scanner(System.in);
-        System.out.print("nota: ");
+        Scanner pago = new Scanner(System.in);
+        float total;
+        System.out.print("horas trabajadas: ");
+        float horas = pago.nextFloat();
+
+        System.out.print("Precio por hora: ");
+        float precio_hora = pago.nextFloat();
+
+        if (horas<35){
+            total= horas * precio_hora;
+            System.out.print("cantidad a pagar: "+ total + "€");
+        } else {
 
         }
     }
+}
 
---
+++ src/com/company/pruebaexamen.java
@@ -0,0 +1,21 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class pruebaexamen {
+
+    public static void main(String[] args) {
+
+        Scanner segundos = new Scanner(System.in);
+        System.out.print("segundos: ");
+        int segundo=segundos.nextInt();
+        int total;
+
+        System.out.println(total=segundo);
+
+
+
+
+    }
+}
+
