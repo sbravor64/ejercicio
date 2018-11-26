9dac5e941a914e7928d3169f93e9f357bd6fd878
Andrés Bravo <sbravir642elpuig.xeill.net>
2018-11-26 23:31:20 +0100

    ejercicio hack 2 hecho

--- src/com/company/ejerciciohack2.java
+++ src/com/company/ejerciciohack2.java
@@ -8,27 +8,28 @@ public class ejerciciohack2 {
 
         Scanner tec = new Scanner(System.in);
         int numt = tec.nextInt();
-        int num = tec.nextInt();
+        int num;
         int numant = 0;
         int cont=0;
         int contt=0;
 
-        if (numt>=0 || numt<=100){
+        if (numt==0){
+            System.out.println("0");
+        } else if (numt>=0 || numt<=100){
             while (numt!=contt) {
-
-                while (num != 0) {
+                num=tec.nextInt();
+                while (num > 0) {
                     numant = num;
                     num = tec.nextInt();
 
-                    if (num != numant) {
+                    if (num != numant || num==numant) {
                         cont++;
                     }
                 }
+                System.out.println(cont);
                 contt++;
+                cont=0;
             }
         }
-
-        System.out.println(cont);
     }
 }
-
