11d19627e5d792358c99a05621af6d774d10650d
Andrés Bravo <sbravir642elpuig.xeill.net>
2018-11-28 01:36:46 +0100

    ejercicio 4 (hecho de otra manera) y ejercicio 5 hecho (pero con algunos errores)

---
+++ src/com/company/ejerciciohack04.java
@@ -0,0 +1,50 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class ejerciciohack04 {
+
+    public static void main(String[] args) {
+
+        Scanner tec = new Scanner(System.in);
+        int num = tec.nextInt();
+        int numd = tec.nextInt();
+        int numant;
+        int total=0;
+        boolean f = true;
+
+
+        if (num==0 && numd==1){
+            f=true;
+            total=tec.nextInt();
+            if(total==0){
+                f=true;
+            }else if (num+numd!=total){
+                f=false;
+            } while (num+numd==total){
+                if (num+numd!=total){
+                    f=false;
+                } else {
+                    f = true;
+                    num = numd;
+                    numd = total;
+                    total = tec.nextInt();
+                    if(total==0){
+                        break;
+                    }
+                    if(num+numd!=total){
+                        f=false;
+                    }
+                }
+
+            }
+        }
+
+        if (f==true){
+            System.out.println("SI");
+        } else if (f==false){
+            System.out.println("NO");
+        }
+
+    }
+}
---
+++ src/com/company/ejerciciohack5.java
@@ -0,0 +1,78 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class ejerciciohack5 {
+
+    public static void main(String[] args) {
+
+        Scanner tec = new Scanner(System.in);
+        int cant=tec.nextInt();
+        int num = tec.nextInt();
+        int numd = tec.nextInt();
+        int numant;
+        int total=0;
+        int cont=0;
+        boolean f = false;
+
+        if(cant==0) {
+            return;
+        } else if (cant>=0 || cant<=100){
+            while (cant!=cont) {
+                if (num == 0 && numd == 1) {
+                    f = true;
+                    total = tec.nextInt();
+                    if (total == 0) {
+                        f = true;
+                        cont++;
+                    } else if (num + numd != total) {
+                        f = false;
+                        cont++;
+                    } while (num+numd==total){
+                        if (num+numd!=total){
+                            f=false;
+                            cont++;
+                        } else {
+                            f = true;
+                            num = numd;
+                            numd = total;
+                            total = tec.nextInt();
+                            if(total==0){
+                                cont++;
+                                break;
+                            }
+                            if(num+numd!=total){
+                                f=false;
+                                cont++;
+                            }
+                        }
+                    }
+                } else{
+                    while(num!=0 || numd!=1){
+                        num=tec.nextInt();
+                        if(num==0 || numd==0){
+                            break;
+                        }
+                    }
+                    f=false;
+                    cont++;
+                }
+
+                if (f==true){
+                    System.out.println("SI");
+                } else if (f==false){
+                    System.out.println("NO");
+                }
+
+                if(total==0){
+                    num=tec.nextInt();
+                    numd=tec.nextInt();
+                }
+
+                if(numd==0){
+                    return;
+                }
+            }
+        }
+    }
+}
