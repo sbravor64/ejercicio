7d7c1bddc37b4580c9ad7f5ac144bc4775bcf3cf
Andrés Bravo <sbravir642elpuig.xeill.net>
2018-12-01 14:02:07 +0100

    ejerciccio 8 empezado

---
+++ src/com/company/ejerciciohack8.java
@@ -0,0 +1,19 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class ejerciciohack8 {
+
+    public static void main(String[] args) {
+
+        Scanner tec = new Scanner(System.in);
+        int num1 = tec.nextInt();
+        int num2,num3,numf;
+        int cont=0;
+
+        if(num1!=0){
+            num2=tec.nextInt();
+
+        } else {}
+    }
+}
