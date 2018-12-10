bbdb59477b523d814a0a9cbb89d49be0cf1b70f1
sbravor64@elpuig.xeill.net <19972208>
2018-12-10 18:31:03 +0100

    creadas carpetas contests

--- src/com/company/contest1/ejerciciohack7.java
+++ src/com/company/contest1/ejerciciohack7.java
@@ -1,4 +1,4 @@
-package com.company;
+package com.company.contest1;
 
 import java.util.Scanner;
 
---
+++ src/com/company/contest3/hack1.java
@@ -0,0 +1,10 @@
+package com.company.contest1;
+
+import java.util.Scanner;
+
+public class hack1 {
+
+    public static void main(String[] args) {
+
+    }
+}
