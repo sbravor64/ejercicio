e9d64e298cea87d0113c1863250152dcfe67eb9c
Andrés Bravo <sbravir642elpuig.xeill.net>
2018-12-12 00:04:19 +0100

    ejercicio 6 de contest3 hecho

--- src/com/company/contest3/hack6.java
+++ src/com/company/contest3/hack6.java
@@ -7,26 +7,30 @@ public class hack6 {
     public static void main(String[] args) {
 
         Scanner tec = new Scanner(System.in);
-        int cant=tec.nextInt(), i,j, total=0;
+        int cant=tec.nextInt(), ultimo, i,j, cont=0;
         boolean igual=false;
         int[] num = new int[cant];
 
         for (i = 0; i < cant; i++) {
             num[i]=tec.nextInt();
         }
-
-        int cant2=tec.nextInt();
-        int[] num2 = new int[cant2];
-
-        for (j = 0; j < cant2; j++) {
-            num2[j]=tec.nextInt();
+        j=0; i=cant-1;
+        while(num[i]==num[j]){
+            igual=true;
+            i--; j++;
+            if(i==j || i<j){
+                break;
+            }
+            if(num[i]!=num[j]){
+                igual=false;
+                break;
+            }
         }
-        i = 0; j = 0;
-        do {
-            total=num[i] + num2[j];
-            System.out.print(total + " ");
-            i++; j++;
 
-        } while(i!=cant && j!=cant2);
+        if(igual==true){
+            System.out.println("SI");
+        } else{
+            System.out.println("NO");
+        }
     }
 }
