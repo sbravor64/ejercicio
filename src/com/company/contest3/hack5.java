286f3a3ab84ae888bc0f0305bfd034f125df989d
Andrés Bravo <sbravir642elpuig.xeill.net>
2018-12-11 23:35:26 +0100

    ejercicio 4 y 5 de contest3 hechos

--- src/com/company/contest3/hack5.java
+++ src/com/company/contest3/hack5.java
@@ -2,12 +2,12 @@ package com.company.contest3;
 
 import java.util.Scanner;
 
-public class hack4 {
+public class hack5 {
 
     public static void main(String[] args) {
 
         Scanner tec = new Scanner(System.in);
-        int cant=tec.nextInt(), ultimo, i,j, cont=0;
+        int cant=tec.nextInt(), i,j, total=0;
         boolean igual=false;
         int[] num = new int[cant];
 
@@ -23,15 +23,10 @@ public class hack4 {
         }
         i = 0; j = 0;
         do {
-            if (cant==cant2 && num[i] == num2[j]) {
-                igual = true;
-            } else {
-                igual = false;
-            }
+            total=num[i] + num2[j];
+            System.out.print(total + " ");
             i++; j++;
 
         } while(i!=cant && j!=cant2);
-
-        System.out.println(igual);
     }
 }
