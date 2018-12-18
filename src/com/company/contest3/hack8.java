0afe8df848f4719d23398d5d21e7d8c51a174c54
sbravor64@elpuig.xeill.net <19972208>
2018-12-18 17:01:12 +0100

    ejercicio 8 de contest 3 empezado

--- src/com/company/contest3/hack8.java
+++ src/com/company/contest3/hack8.java
@@ -17,12 +17,25 @@ public class hack8 {
             }
         }
 
-        for (int i = 0; i <columnas ; i++) {
-            for (int j = 0; j <filas ; j++) {
-                System.out.print(tablero[i][j]+" ");
-            }
-            System.out.println(" ");
-        }
+        //for (int i = 0; i <columnas ; i++) {
+        //    for (int j = 0; j <filas ; j++) {
+        //        System.out.print(tablero[i][j]+" ");
+        //    }
+        //    System.out.println(" ");
+        //}
 
+        int columna=tec.nextInt(), fila=tec.nextInt();
+
+        while (columna!=0 && fila!=0){
+            for (int i = 0; i <columnas ; i++) {
+                for (int j = 0; j <filas ; j++) {
+                    if(tablero[i][j]==1){
+                        System.out.println("SI");
+                    }
+                }
+            }
+            columna=tec.nextInt();
+            fila=tec.nextInt();
+        }
     }
 }
