d49e3cf6a5dce3bcf2c712a657401a18d595ee53
sbravor64@elpuig.xeill.net <19972208>
2019-01-22 17:52:39 +0100

    contest6 empezado

--- src/com/company/contest6/ejercicio6.java
+++ src/com/company/contest6/ejercicio6.java
@@ -8,20 +8,15 @@ public class ejercicio6 {
         Scanner tec = new Scanner(System.in);
         String frase=tec.nextLine();
         String letra=tec.nextLine();
-        boolean pali= false;
+        boolean pali= true;
 
         for (int i = 0; i <frase.length() ; i++) {
-            int carac=letra.length();
+            char carac=letra.charAt(0);
             char carac1=frase.charAt(i);
-            if(carac!=1){
-                String frase2=letra;
-                letra=tec.nextLine();
-            } else {
 
                 if(carac1==carac){
-                    pali=true;
+                    pali=false;
                 }
-            }
         }
 
         if(pali==true){
---
+++ src/com/company/contest7/ejercicio1.java
@@ -0,0 +1,17 @@
+package com.company.contest7;
+
+import java.util.Scanner;
+
+public class ejercicio1 {
+
+    public static void main(String[] args) {
+        Scanner tec = new Scanner(System.in);
+        String num =tec.nextLine();
+            for (int i = 0; i <num.length() ; i++) {
+                char num1=num.charAt(i);
+
+            }
+
+    }
+}
+
