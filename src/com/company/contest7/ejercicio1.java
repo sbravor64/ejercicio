ce0dc7ce02475c6a78590e533aeac5bbe43cc0f2
Andrés Bravo <sbravir642elpuig.xeill.net>
2019-01-23 11:38:11 +0100

    contest 7 empezado

--- src/com/company/contest7/ejercicio1.java
+++ src/com/company/contest7/ejercicio1.java
@@ -7,11 +7,21 @@ public class ejercicio1 {
     public static void main(String[] args) {
         Scanner tec = new Scanner(System.in);
         String num =tec.nextLine();
+        int total=0;
+
             for (int i = 0; i <num.length() ; i++) {
-                char num1=num.charAt(i);
+                char carac = num.charAt(i);
+                if(carac!=' '){
+                    int carac1= Integer.parseInt(String.valueOf(num.charAt(i)));
+                    total += carac1;
 
+                } else if (carac==' ') {
+                System.out.println(total);
+                total=0;
+                } else if (carac==0){
+                    return;
+                }
             }
-
     }
 }
 
