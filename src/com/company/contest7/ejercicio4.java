3aa505f5947cb775e28a4a9461629742f318624b
sbravor64@elpuig.xeill.net <19972208>
2019-02-05 17:47:43 +0100

    contest7 empezado

--- src/com/company/contest7/ejercicio4.java
+++ src/com/company/contest7/ejercicio4.java
@@ -6,16 +6,32 @@ public class ejercicio4 {
 
     public static void main(String[] args) {
         Scanner tec = new Scanner(System.in);
-        String juego = tec.nextLine();
-        String [][] matriz = new String[8][8];
+        int Bi,Bj, Ni, Nj;
+        char [][] matriz = new char[8][8];
 
-        for (int i = 0; i <=8 ; i++) {
-            for (int j = 0; j <=8 ; j++) {
-                char carac=juego.charAt(j);
+        for (int i = 0; i <matriz.length ; i++) {
+            String juego = tec.nextLine();
+            for (int j = 0; j <matriz[i].length ; j++) {
+                char carac = juego.charAt(j);
+                matriz [i][j]= carac;
 
-                System.out.print(matriz[i][j]);
+                if(matriz [i][j]=='B'){
+                    Bi=i;
+                    Bj=j;
+                } else if(matriz [i][j]=='N'){
+                    Ni=i;
+                    Nj=j;
+                }
             }
         }
+
+        if(Bi==Ni){
+
+        }
+
+
+
+
     }
 }
 
--- src/com/company/contest7/ejercicio5.java
+++ src/com/company/contest7/ejercicio5.java
@@ -9,7 +9,7 @@ public class ejercicio5 {
 
     public static void main(String[] args) {
         Scanner tec = new Scanner(System.in);
-        char [][] matriz = new char[3][3];
+        char [][] matriz = new char[3][3];   // {}
         int cont=0;
         
         for (int i = 0; i <matriz.length ; i++) {
@@ -20,20 +20,57 @@ public class ejercicio5 {
             }
         }
 
-        //if(matriz[0][0]=='O' && matriz[0][1]=='O' && matriz[0][2]=='O'){
-        //    System.out.println('O');
-        //} else if(matriz[0][0]=='X' && matriz[0][1]=='X' && matriz[0][2]=='X'){
-        //    System.out.println('X');
-        //}
-
-        for (int i = 0; i <matriz.length ; i++) {
-            for (int j = 0; j <matriz[i].length ; j++) {
-                if (matriz[i][j]=='O'){
-
-                }
+        if(matriz[0][0]==matriz[0][1] && matriz[0][1]==matriz[0][2]){
+            if(matriz[0][0]=='O'){
+                System.out.println('O');
+            } else if(matriz[0][0]=='X'){
+                System.out.println('X');
             }
+        } else if(matriz[1][0]==matriz[1][1] && matriz[1][1]==matriz[1][2]){
+            if(matriz[1][0]=='O'){
+                System.out.println('O');
+            } else if(matriz[1][0]=='X'){
+                System.out.println('X');
+            }
+        } else if(matriz[2][0]==matriz[2][1] && matriz[2][1]==matriz[2][2]){
+            if(matriz[2][0]=='O'){
+                System.out.println('O');
+            } else if(matriz[2][0]=='X'){
+                System.out.println('X');
+            }
+        } else if(matriz[0][0]==matriz[1][0] && matriz[1][0]==matriz[2][0]){
+            if(matriz[0][0]=='-'){
+                System.out.println('O');
+            } else if(matriz[0][0]=='X'){
+                System.out.println('X');
+            }
+        } else if(matriz[0][1]==matriz[1][1] && matriz[1][1]==matriz[2][1]){
+            if(matriz[0][1]=='O'){
+                System.out.println('O');
+            } else if(matriz[0][1]=='X'){
+                System.out.println('X');
+            }
+        } else if(matriz[0][2]==matriz[1][2] && matriz[1][2]==matriz[2][2]){
+            if(matriz[0][2]=='O'){
+                System.out.println('O');
+            } else if(matriz[0][2]=='X'){
+                System.out.println('X');
+            }
+        } else if(matriz[0][0]==matriz[1][1] && matriz[1][1]==matriz[2][2]){
+            if(matriz[0][0]=='O'){
+                System.out.println('O');
+            } else if(matriz[0][0]=='X'){
+                System.out.println('X');
+            }
+        } else if(matriz[0][2]==matriz[1][1] && matriz[1][1]==matriz[2][0]){
+            if(matriz[0][2]=='O'){
+                System.out.println('O');
+            } else if(matriz[0][2]=='X'){
+                System.out.println('X');
+            }
+        } else {
+            System.out.println('-');
         }
-
     }
 }
 
